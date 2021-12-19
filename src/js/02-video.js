787ecb1de69fa69d82976bf9232c10fae859bdd9
AleksandrTatarenko <78167369+AleksandrTatarenko@users.noreply.github.com>
2021-12-19 21:15:18 +0200

    homework 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from "lodash/throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(videoStart, 1000)); 
+        
+        player.getCurrentTime().then((sec) => {
+            console.log(sec);
+            localStorage.setItem("videoplayer-current-time", JSON.stringify(sec));
+        });
+   
+
+const timeToStart =
+    JSON.parse(localStorage.getItem("videoplayer-current-time")) || 0;
+console.log(timeToStart);
+
+function videoStart(timeToStart) {
+    console.log(timeToStart)
+}
