e10f3fa3d28ac25ebb8d5012618615ca679ef112
AleksandrTatarenko <78167369+AleksandrTatarenko@users.noreply.github.com>
2021-12-19 19:32:44 +0200

    homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createPictureCard(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`;
+        })
+        .join('');
+}
+
+import SimpleLightbox from "simplelightbox";
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+import 'simplelightbox/dist/simple-lightbox.min.css'
